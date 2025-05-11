e3aafcccd2d63ddcf70df5f2b080bf4f96f5c07b
assembler-0 <diaviekone13@gmail.com>
2025-05-11 10:00:46 +0700

    edit .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -43,7 +43,7 @@ void init_system_resource_manager(int argc, char** argv){
   std::cout << "The 'rm' (Resource Manager) command is not yet implemented." << std::endl;
   return;
 }
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
   CLI::App smctl{"smctl - A Powerful system manager for Arch-based distro"};
   smctl.name("smctl");
   smctl.set_version_flag("-v,--version", "smctl version 2.0 - Remastered", "Display version information");
@@ -57,10 +57,30 @@ int main(int argc, char **argv){
   hpc_subcommand->callback([argc, argv](){
       init_calculator_high_precision(argc, argv);
   });
-  CLI::App* ccleaner_subcommand = smctl.add_subcommand("cc", "Cache and garbage collection for pacman and marked git repos");
+  CLI::App* ccleaner_subcommand = smctl.add_subcommand("cc", "Cache and garbage collection for pacman and marked git repos, remove unused packages");
   ccleaner_subcommand->fallthrough();
   ccleaner_subcommand->callback([argc, argv]() {
       init_cache_cleaner(argc, argv);      
+  });
+  CLI::App* marker_subcommand = smctl.add_subcommand("mark", "Mark files for recalling and garbage collection for git repos");
+  marker_subcommand->fallthrough();
+  marker_subcommand->callback([argc, argv]() {
+      init_file_marker(argc, argv);
+  });
+  CLI::App* autoupdate_subcommand = smctl.add_subcommand("au", "Running pacman -Syu and mkinitcpio as a service");
+  autoupdate_subcommand->fallthrough();
+  autoupdate_subcommand->callback([argc, argv]() {
+      init_auto_update(argc, argv);
+  });
+  CLI::App* autotune_subcommand = smctl.add_subcommand("autotune,at", "Auto tune your system for better power efficientcy and performance");
+  autotune_subcommand->fallthrough();
+  autotune_subcommand->callback([argc, argv]() {
+      init_system_autotune(argc, argv);
+  });
+  CLI::App* resmgr_subcommand = smctl.add_subcommand("resmgr,rm", "Show temps, cpu, gpu, ram usage");
+  resmgr_subcommand->fallthrough();
+  resmgr_subcommand->callback([argc, argv]() {
+      init_system_resource_manager(argc, argv);
   });
   try {
     smctl.parse(argc, argv);
